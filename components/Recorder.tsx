9d3ca76c11210ea176dc14169655b3b704f67f15
Rokas <rookas.rudzenskas@gmail.com>
2024-03-17 16:02:24 +0200

    media recorder stream functions

--- components/Recorder.tsx
+++ components/Recorder.tsx
@@ -18,17 +18,67 @@ const Recorder = ({}) => {
   const [audioChunks, setAudioChunks] = useState<Blob[]>([]);
   const [audio, setAudio] = useState<string | null>(null);
 
-  const getMicrophonePermission = async () => {
+  useEffect(() => {
+    getMicrophonePermission();
+  }, []);
 
-  }
+  const getMicrophonePermission = async () => {
+    if ("MediaRecorder" in window) {
+      try {
+        const streamData = await navigator.mediaDevices.getUserMedia({
+          audio: true,
+          video: false,
+        });
+        setPermission(true);
+        setStream(streamData);
+      } catch (err: any) {
+        alert(err.message);
+      }
+    } else {
+      alert("The MediaRecorder API is not supported in your browser.");
+    }
+  };
 
   const startRecording = async () => {
+    if (mediaRecorder === null || stream === null) return;
 
-  }
+    if (pending) return;
 
-  const stopRecording = async () => {
+    setRecordingStatus("recording");
+    //create new Media recorder instance using the stream
+    const media = new MediaRecorder(stream, { mimeType: mimeType });
+    //set the MediaRecorder instance to the mediaRecorder ref
+    mediaRecorder.current = media;
+    //invokes the start method to start the recording process
+    mediaRecorder.current.start();
+    let localAudioChunks: Blob[] = [];
+    mediaRecorder.current.ondataavailable = (event) => {
+      if (typeof event.data === "undefined") return;
+      if (event.data.size === 0) return;
+      localAudioChunks.push(event.data);
+    };
+    setAudioChunks(localAudioChunks);
+  };
 
-  }
+
+  const stopRecording = () => {
+    if (mediaRecorder.current === null) return;
+
+    if (pending) return;
+
+    setRecordingStatus("inactive");
+    //stops the recording instance
+    mediaRecorder.current.stop();
+    mediaRecorder.current.onstop = () => {
+      //creates a blob file from the audiochunks data
+      const audioBlob = new Blob(audioChunks, { type: mimeType });
+      //creates a playable URL from the blob file.
+      const audioUrl = URL.createObjectURL(audioBlob);
+      setAudio(audioUrl);
+      uploadAudio(audioBlob);
+      setAudioChunks([]);
+    };
+  };
 
   return (
     <div className="flex items-center justify-center">
